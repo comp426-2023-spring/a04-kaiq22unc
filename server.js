71fa87938a420e67829db9f4f0c90693673cca13
kaiq913 <77793337+kaiq913@users.noreply.github.com>
2023-05-06 18:07:23 -0400

    app

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-const http = require('http');
-const url = require('url');
-const PORT = process.argv[2] || 5000;
+#!/usr/bin/env node
 
-const appEndpoint = '/app/';
+import minimist from 'minimist';
+import express from 'express';
+import {rps, rpsls} from './lib/rpsls.js';
 
-const server = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true);
+const argv = minimist(process.argv.slice(2));
+const app = express();
+const port = process.env.PORT || 5000;
 
-  if (reqUrl.pathname === appEndpoint) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('200 OK\n');
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('404 NOT FOUND\n');
-  }
+
+// Default endpoint for undefined routes
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found');
 });
+
+// Check endpoint
+app.get('/app', (req, res) => {
+  res.status(200).send('200 OK');
+});
+
+app.get('app/rps', (req, res) => {
+  res.status(200).json(rps());
+}
+app.get('app/rpsls', (req, res) => {
+  res.status(200).json(rpsls());
+}
+
+
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
